c087f5a795c36cebbd0458c35ffb613fe0b0f59a
Kala30 <caleb.yun@gmail.com>
2021-05-14 19:42:41 +0100

    Added DFS and BFS

--- BaseballTour/graph.h
+++ BaseballTour/graph.h
@@ -3,20 +3,28 @@
 
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <limits.h>
 
 using namespace std;
 
 /**
- * @brief Graph class based on adjacency matrix
+ * @brief Graph class based on undirected adjacency matrix
  */
 
-// struct representation of edge in graph
+/**
+ * @brief The Edge struct
+ */
 struct Edge {
 
-    // index of first node
+    /**
+     * @brief u index of first node
+     */
     int u;
 
-    // index of second node
+    /**
+     * @brief v index of second node
+     */
     int v;
 
     int weight;
@@ -29,6 +37,16 @@ public:
     Graph();
 
     /**
+     * @brief dfsOrder Order of nodes after DFS is run
+     */
+    vector<Type> dfsOrder;
+
+    /**
+     * @brief bfsOrder Order of nodes after BFS is run
+     */
+    vector<Type> bfsOrder;
+
+    /**
      * @brief Adds node to graph
      * @param obj Object to add to graph
      */
@@ -36,20 +54,27 @@ public:
 
     /**
      * @brief Add edge between two nodes
-     * @param objU
-     * @param objV
-     * @param weight
+     * @param objU Name of first node
+     * @param objV Name of second node
+     * @param weight Weight of edge
      */
     void addEdge(Type& objU, Type& objV, int weight);
 
-private:
+    /**
+     * @brief Starts DFS and populates dfsOrder
+     * @param start Starting node
+     * @return Total cost (distance)
+     */
+    int startDFS(Type& start);
 
     /**
-     * @brief Helper function to get index of obj
-     * @param obj object to be found
-     * @return index of obj
+     * @brief Starts BFS and populates bfsOrder
+     * @param start Starting node
+     * @return Total cost (distance)
      */
-    int getIndex(Type& obj);
+    int startBFS(Type& start);
+
+private:
 
     /**
      * @brief size number of nodes
@@ -61,11 +86,35 @@ private:
      */
     vector<vector<Edge>> adjMatrix;
 
-
     /**
      * @brief List of data associated with index
      */
     vector<Type> nodeList;
+
+    /**
+     * @brief Helper function to get index of node
+     * @param obj Object to be found
+     * @return Index of obj
+     */
+    int getIndex(Type& obj);
+
+    /**
+     * @brief DFS recursive function
+     * @param v Starting node index
+     * @param visited Keeps track of each visited node
+     * @return Total cost (distance)
+     */
+    int dfs(int v, vector<bool>& visited);
+
+    /**
+     * @brief Compares Edges (sorts in increasing order)
+     * @param a Edge a
+     * @param b Edge b
+     * @return true if Edge a is less than b
+     */
+    static bool compareWeight(const Edge& a, const Edge& b) {
+        return a.weight < b.weight;
+    }
 
 };
 
