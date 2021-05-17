91f9fcdc35a9a29bb5356c4d9f1b4c974be4107a
Kala30 <caleb.yun@gmail.com>
2021-05-17 04:11:48 +0100

    Updated QSS, font, added icons

--- BaseballTour/main.cpp
+++ BaseballTour/main.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QFontDatabase>
+
+QByteArray readTextFile(const QString &file_path) {
+  QFile input_file(file_path);
+  QByteArray input_data;
+
+  if (input_file.open(QIODevice::Text | QIODevice::Unbuffered | QIODevice::ReadOnly)) {
+    input_data = input_file.readAll();
+    input_file.close();
+    return input_data;
+  }
+  else {
+    return QByteArray();
+  }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString style_sheet = readTextFile(":/stylesheets/MaterialDark.qss");
+    a.setStyleSheet(style_sheet);
+    QFontDatabase::addApplicationFont(":/font/Roboto.ttf");
+    QFont roboto("Roboto", 12);
+    a.setFont(roboto);
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
